ce1af0d184b8650db4663431b80a9580360cbeec
Raj Vishwakarma <the.raj2301@gmail.com>
2026-02-25 10:50:32 +0530

    Designing City Dao

---
+++ worldgdp/src/main/java/com/raj/worldgdp/dao/CityDao.java
@@ -0,0 +1,85 @@
+package com.raj.worldgdp.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Service;
+
+import com.raj.worldgdp.dao.mapper.CityRowMapper;
+import com.raj.worldgdp.model.City;
+
+import lombok.Setter;
+
+@Service
+@Setter
+public class CityDao {
+	
+	@Autowired
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	private static final Integer PAGE_SIZE = 20;
+	
+	public List<City> getCities(String coutryCode,  Integer pageNo){
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("code", coutryCode);
+		
+		Integer offset = (pageNo - 1) * PAGE_SIZE;
+		params.put("offset", offset);
+		params.put("size", PAGE_SIZE);
+		
+		return namedParameterJdbcTemplate.query(
+				"SELECT "
+				+ " id, name, countryCode country_code, district, population from city "
+				+ " WHERE code = :code "
+				+ " ORDER BY PUPULATION DESC "
+				+ ((pageNo != null) ? " LIMIT = :offset, :size " : "")
+				, params, new CityRowMapper());
+	}
+	
+	public List<City> getCities(String country){
+		return getCities(country, null);
+	}
+	
+	public City getCityDetails(Long id) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		return namedParameterJdbcTemplate.queryForObject(
+				" SELECT name, countryCode country_code, district, population "
+				+ " FROM city WHERE id = :id"
+				, params, new CityRowMapper());
+	}
+	
+	public Long addCity(String countryCode, City city) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("name", city.getName());
+		params.put("country_code", countryCode);
+		params.put("district", city.getDistrict());
+		params.put("population", city.getPopulation());
+		
+		SqlParameterSource sqlParameterSource = new MapSqlParameterSource(params);
+		
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		
+		namedParameterJdbcTemplate.update(
+				" INSERT INTO city (name, countryCode country_code, district, population)"
+				+ " VALUES (:name, :country_code, :district, :population) "
+				, sqlParameterSource, keyHolder);
+		
+		return keyHolder.getKey().longValue();
+	}
+	
+	public void deleteCity(Long id) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", id);
+		
+		namedParameterJdbcTemplate.update("DELETE FROM CITY WHERE id = :id", params);
+	}
+
+}
---
+++ worldgdp/src/test/java/com/raj/worldgdp/dao/CityDaoTest.java
@@ -0,0 +1,94 @@
+package com.raj.worldgdp.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.raj.worldgdp.config.TestDBConfiguration;
+import com.raj.worldgdp.model.City;
+
+@Transactional
+@ExtendWith(SpringExtension.class)
+@SpringJUnitConfig(classes = {TestDBConfiguration.class, CityDaoTest.class})
+class CityDaoTest {
+	
+	@Autowired
+	CityDao cityDao;
+	
+	@Autowired
+	@Qualifier("testTemplate")
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	@BeforeEach
+	public void setUp() {
+		cityDao.setNamedParameterJdbcTemplate(namedParameterJdbcTemplate);
+	}
+	
+	@Test
+	public void testGetCities() {
+		List<City> cities = cityDao.getCities("IND", 1);
+		assertThat(cities).hasSize(10);
+	}
+	
+	@Test
+	public void testGetCityDetails() {
+		City cityDetails = cityDao.getCityDetails(1024L);
+		assertThat(cityDetails.getName()).isEqualTo("Mumbai(Bombay)");
+		assertThat(cityDetails.getCountryCode()).isEqualTo("IND");
+		assertThat(cityDetails.getDistrict()).isEqualTo("Maharastra");
+		assertThat(cityDetails.getPopulation()).isEqualByComparingTo(10500000L);
+	}
+	
+	@Test
+	public void testAddCity() {
+		
+		String countryCode = "IND";
+		
+		City c = new City();
+		c.setName("City Name");
+		c.setDistrict("City District");
+		c.setPopulation(1000L);
+		
+		Long cityId = cityDao.addCity(countryCode, c);
+		assertThat(cityId).isNotNull();
+		
+		City cityDetails = cityDao.getCityDetails(cityId);
+		assertThat(cityDetails.getId()).isNotNull();
+		assertThat(cityDetails.getName()).isEqualTo(c.getName());
+		assertThat(cityDetails.getDistrict()).isEqualTo(c.getDistrict());
+		assertThat(cityDetails.getPopulation()).isEqualTo(c.getPopulation());
+	}
+	
+	@Test
+	public void testDeleteCity() {
+		Long cityId = addCity();
+		cityDao.deleteCity(cityId);
+//		assertThat(cityDao.getCityDetails(cityId)).isNull();
+		assertThatThrownBy(() -> cityDao.getCityDetails(cityId)).isInstanceOf(EmptyResultDataAccessException.class);
+	}
+	
+	private Long addCity() {
+		City c = new City();
+		c.setName("City");
+		c.setDistrict("District");
+		c.setPopulation(101010L);
+		Long cityId = cityDao.addCity("IND", c);
+		return cityId;
+	}
+
+}
